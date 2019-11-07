74773490adc327d0efb16f7d48869a59f6cd27fb
pugbabu <243931551@qq.com>
2019-11-07 09:30:15 +0800

    commit

--- src/store/init-store.js
+++ src/store/init-store.js
@@ -4,7 +4,7 @@ import store from './index';
 export default function initStore() {
   store.commit('setUser', Vue.ss.get('user'));
   store.commit('setToken', Vue.ss.get('token'));
-  store.commit('setOpLogs', Vue.ss.get('opLogs'));
+  store.commit('setOpLogs', Vue.ss.get('opLogs') || []);
 
 
 }
